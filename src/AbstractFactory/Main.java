b2ae663fbeed71cae79889ccfa87e2b109385832
antonwis <56794229+antonwis@users.noreply.github.com>
2020-03-31 00:22:41 +0300

    reflection bonus

--- src/AbstractFactory/Main.java
+++ src/AbstractFactory/Main.java
@@ -1,20 +1,48 @@
 package AbstractFactory;
 
 import AbstractFactory.Factories.AbstractFactory;
-import AbstractFactory.Factories.AdidasFactory;
-import AbstractFactory.Factories.BossFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        AbstractFactory adidasFactory = new AdidasFactory();
-        AbstractFactory bossFactory = new BossFactory();
-        System.out.println("Jasperin vaatteet opiskelijana:");
-        System.out.println(adidasFactory.toString());
-        System.out.println("Jasperin vaatteet insinöörinä:");
-        System.out.println(bossFactory.toString());
+        Class c = null;
+        AbstractFactory factory = null;
 
+        Properties properties = new Properties();
+        try {
+            InputStream inputStream = Main.class.getResourceAsStream("factory.properties");
+            properties.load(inputStream);
+        }
+        catch (IOException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+
+        try {
+            c = Class.forName(properties.getProperty("adidas"));
+            factory = (AbstractFactory)c.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Jasperin vaatteet opiskelijana:");
+        System.out.println(factory.toString());
+
+        try {
+            c = Class.forName(properties.getProperty("boss"));
+            factory = (AbstractFactory)c.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Jasperin vaatteet insinöörinä:");
+        System.out.println(factory.toString());
 
     }
 }
